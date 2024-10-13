e79d4a7a3c32779cb8969ec484bcd2786e6d00e1
Lavra Tamutus <lavratamutus@gmail.com>
2024-10-13 15:36:19 -0400

    Init Nuxt app

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <p>[Summary of DDT and what we offer]</p>
+    <p>Services:</p>
+    <p>Automations</p>
+    <p>Free examples / instructions (created from facebook post content)</p>
+    <p>(I’m not sure the best way to organize these)</p>
+    <p>“Click Here for Automations Help” link to TPA link</p>
+    <p>Help with organizing and/or understanding your business data</p>
+    <p>Free Excel template download</p>
+    <p>Instructions on how to use the Excel template</p>
+    <p>
+      (Note: I still need to create these, either written or video. I’m thinking
+      written, so I can use this for facebook posts as well.)
+    </p>
+    <p>Google form for expression of interest in data help</p>
+    <p>(coming soon) Receipt parser and pdf filler</p>
+    <p>Google form link for expression of future interest</p>
+    <p>(coming soon) medication scheduler and refill reminder</p>
+    <p>Google form link for expression of future interest</p>
+
+    <p>Colors: easy, light blue focused</p>
+    <p>Warm, friendly, helpful</p>
+
+    <!-- <NuxtWelcome /> -->
+  </div>
+</template>
